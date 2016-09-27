8643f82adcc77076af476d4f430ef6e01d8df9ad
Flatiron School <flatironschool@Flatirons-MacBook-Air.local>
2016-09-27 18:21:18 -0400

    Small change

--- Icecream/Icecream.swift
+++ Icecream/Icecream.swift
@@ -36,6 +36,7 @@ class Icecream {
             numberOfPeopleWhoLikeThatFlavor += 1
         }
     }
+        
         print(numberOfPeopleWhoLikeThatFlavor)
         return numberOfPeopleWhoLikeThatFlavor
     }
